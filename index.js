028c7f52239a1dc0bf14785f9e04b022a04a9227
Abbiechebet <abigaelchebet85@gmail.com>
2023-02-20 12:23:15 +0300

    another commit

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 //problem statement for question 3
 
 /**
- * Bolatito just got admitted to Greenfield High School as an Arts student. To commence 
- * her studies, she needs to know the subjects she will be taking as an Arts student. All 
- * students have to take the General subjects.
-	
-The subjects for each class group are as follows:
-	Science Subjects - Physics, Chemistry, Biology, Technical Drawing 
+ *The subjects for each class group are as follows:
+	  Science Subjects - Physics, Chemistry, Biology, Technical Drawing 
     Social Science Subjects - Accounting, Commerce, Marketing, Geography
     Arts Subjects - Government, Economics, Literature, History
     General Subjects - English, Mathematics
 
-Using if…else conditional statement, write a program that will help Bolatito determine the 
-subjects she will be taking for the session using her class group. Also, in the case of an 
-invalid class group, your output should be the General subjects. 
-
+ if…else conditional statement
  */
 
 //solution
